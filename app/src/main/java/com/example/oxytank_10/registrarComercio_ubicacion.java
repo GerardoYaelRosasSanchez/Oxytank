baf5558ecd2bc0abdea8f1205749cc42aa0ab991
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-14 22:29:37 -0500

    Agregar la actividad registrarComerci_ubicacion y agregar permisos en el GPS para el manifest

---
+++ app/src/main/java/com/example/oxytank_10/registrarComercio_ubicacion.java
@@ -0,0 +1,13 @@
+package com.example.oxytank_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registrarComercio_ubicacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_comercio_ubicacion);
+    }
+}
