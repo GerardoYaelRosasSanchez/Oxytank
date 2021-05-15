af32e6d9fd1d18d771fceca7c17de2e33abfb3e6
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-15 17:27:48 -0500

    Acomodar el diseño a una pantalla de 1080x2340px, organizar el codigo, crear los intent de navegacion de los botones de registrar comercio

--- app/src/main/java/com/example/oxytank_10/registrarComercio_comercio.java
+++ app/src/main/java/com/example/oxytank_10/registrarComercio_comercio.java
@@ -2,7 +2,9 @@ package com.example.oxytank_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class registrarComercio_comercio extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class registrarComercio_comercio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_comercio_comercio);
     }
+
+    //Métodos.
+
+    //Ir a la pantalla Registrar Comercio: Ubicación.
+    public void registrarComercio_ubicacion(View view){
+        //Pasar a la actividad "registrarComercio_ubicacion".
+        Intent Act_registrarComercio_ubicacion = new Intent(this, registrarComercio_ubicacion.class);
+        startActivity(Act_registrarComercio_ubicacion);
+    }
+
 }
--- app/src/main/java/com/example/oxytank_10/registrarComercio_usuario.java
+++ app/src/main/java/com/example/oxytank_10/registrarComercio_usuario.java
@@ -2,7 +2,9 @@ package com.example.oxytank_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class registrarComercio_usuario extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class registrarComercio_usuario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_comercio_usuario);
     }
+
+    //Métodos.
+
+    //Ir a la pantalla Registrar Comercio: Datos Comercio.
+    public void registrarComercio_comercio(View view){
+        //Pasar a la actividad "registrarComercio_comercio".
+        Intent Act_registrarComercio_comercio = new Intent(this, registrarComercio_comercio.class);
+        startActivity(Act_registrarComercio_comercio);
+    }
+
 }
