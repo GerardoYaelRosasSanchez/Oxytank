73f8f7e0b00fcaecbe1e938774e2317653142d5c
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-16 00:01:26 -0500

    Agregar los permisos para utilizar el fragmento de Google Maps

--- app/src/main/java/com/example/oxytank_10/registrarComercio_ubicacion.java
+++ app/src/main/java/com/example/oxytank_10/registrarComercio_ubicacion.java
@@ -1,7 +1,14 @@
 package com.example.oxytank_10;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.media.audiofx.BassBoost;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
 import com.example.oxytank_10.databinding.ActivityRegistrarComercioUbicacion3Binding;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,6 +17,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.SettingsClickListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class registrarComercio_ubicacion extends AppCompatActivity implements OnMapReadyCallback  {
 
@@ -28,7 +42,27 @@ public class registrarComercio_ubicacion extends AppCompatActivity implements On
 
     private void revisarPermiso() {
 
-        
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            //Si el usuario acepta el permiso.
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                Toast.makeText(registrarComercio_ubicacion.this, "Permiso", Toast.LENGTH_SHORT).show();
+            }
+            //Si el usuario no acepta el permiso.
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(),"");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+            //Si el usuario no selecciona ninguna opción.
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
 
     }
 
