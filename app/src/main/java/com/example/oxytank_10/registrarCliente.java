694bb459563ecf6844684b101cdc3ed30ca44c65
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-23 23:19:34 -0500

    Se implemento la navegación entre las pantallas de registro.

--- app/src/main/java/com/example/oxytank_10/registrarCliente.java
+++ app/src/main/java/com/example/oxytank_10/registrarCliente.java
@@ -2,7 +2,9 @@ package com.example.oxytank_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class registrarCliente extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class registrarCliente extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_cliente);
     }
+
+    //Métodos.
+
+    //Ir a la pantalla Registrar Cliente.
+    public void registrarCliente_comprobarDatos(View view){
+        //Pasar a la actividad "registrarCliente_comprobarDatos".
+        Intent Act_registrarCliente_comprobarDatos = new Intent(this, registrarCliente_comprobarDatos.class);
+        startActivity(Act_registrarCliente_comprobarDatos);
+    }
+
 }
--- app/src/main/java/com/example/oxytank_10/registrarCliente_comprobarDatos.java
+++ app/src/main/java/com/example/oxytank_10/registrarCliente_comprobarDatos.java
@@ -2,7 +2,9 @@ package com.example.oxytank_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class registrarCliente_comprobarDatos extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class registrarCliente_comprobarDatos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_cliente_comprobar_datos);
     }
+
+    //Métodos.
+
+    //Ir a la pantalla principal de la cuenta de tipo cliente.
+    public void cuentaTipoCliente_PantallaPrincipal(View view){
+        //Pasar a la actividad "cuentaTipoCliente_PantallaPrincipal".
+        Intent Act_cuentaTipoCliente_PantallaPrincipal = new Intent(this, cuentaTipoCliente_PantallaPrincipal.class);
+        startActivity(Act_cuentaTipoCliente_PantallaPrincipal);
+    }
+
 }
