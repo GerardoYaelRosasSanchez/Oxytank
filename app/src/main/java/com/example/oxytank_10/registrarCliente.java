b40bf4601234ab22137713103cdb66a389d4c933
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-24 21:16:31 -0500

    Se agrego la conexión con la base de datos en las pantallas de registrar cliente. El registro de cliente se encuentra completo.

--- app/src/main/java/com/example/oxytank_10/registrarCliente.java
+++ app/src/main/java/com/example/oxytank_10/registrarCliente.java
@@ -2,25 +2,130 @@ package com.example.oxytank_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class registrarCliente extends AppCompatActivity {
+
+    //Relación con el entorno gráfico
+    EditText edt_nombreUsuario, edt_correo, edt_contrasenia, edt_verificarContrasenia;
+
+    //Cambio Actividad
+    private Boolean cumpleRequisitos = false;
+
+    // Guardar el ID correspondiente al usuario.
+    int numUsuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_cliente);
+
+        // Relación de las variables con la parte gráfica.
+        edt_nombreUsuario = findViewById(R.id.edt_registrarCliente_nombreUsuario);
+        edt_correo = findViewById(R.id.edt_registrarCliente_correo);
+        edt_contrasenia = findViewById(R.id.edt_registrarCliente_contrasenia);
+        edt_verificarContrasenia = findViewById(R.id.edt_registrarCliente_verificarContrasenia);
+
     }
 
     //Métodos.
 
     //Ir a la pantalla Registrar Cliente.
     public void registrarCliente_comprobarDatos(View view){
+
+        //Agregar del usuario en la base de datos.
+        agregarDatosUsuarioBD();
+
         //Pasar a la actividad "registrarCliente_comprobarDatos".
-        Intent Act_registrarCliente_comprobarDatos = new Intent(this, registrarCliente_comprobarDatos.class);
-        startActivity(Act_registrarCliente_comprobarDatos);
+        if(cumpleRequisitos){
+            //Pasar a la actividad "registrarCliente_comprobarDatos".
+            Intent Act_registrarCliente_comprobarDatos = new Intent(this, registrarCliente_comprobarDatos.class);
+            String IdUsuario_String = Integer.toString(numUsuarios);
+            Act_registrarCliente_comprobarDatos.putExtra("usuario_id", IdUsuario_String);
+            startActivity(Act_registrarCliente_comprobarDatos);
+        }
+
+    }
+
+    // Agrega los datos del usuario en la base de datos.
+    public void agregarDatosUsuarioBD(){
+
+        //Objeto: Administrar la base de datos.
+        DBHelper admin = new DBHelper(this, "Oxytank_db", null, 1);
+
+        // Abrir la base de datos en modo lectura y escritura.
+        SQLiteDatabase BaseDatos = admin.getWritableDatabase();
+
+        // Guardar la información ingresada por el usuario.
+        String nombreUsuario = edt_nombreUsuario.getText().toString();
+        String correo = edt_correo.getText().toString();
+        String contrasenia = edt_contrasenia.getText().toString();
+        String verificarContrasenia = edt_verificarContrasenia.getText().toString();
+        String tipoUsuario = "Cliente";
+
+        //Contar cuantas filas tiene la tabla para crear el id.
+        String contarUsuarios_Consulta = "SELECT nombreUsuario FROM usuarios"; // Guardar el texto que corresponde a la consulta.
+        Cursor cursor = BaseDatos.rawQuery(contarUsuarios_Consulta, null); //Realizar la consulta en la base de datos.
+        numUsuarios = cursor.getCount(); // Guardar el resultado de la consulta en una variable.
+        cursor.close();
+
+        // En base al numero de usuarios, crear el ID de usuario.
+        numUsuarios += 1;
+
+        //Verificar que el usuario haya ingresado datos en los campos.
+        if(!nombreUsuario.isEmpty() && !correo.isEmpty() && !contrasenia.isEmpty() && !verificarContrasenia.isEmpty()){
+
+            // Comprobar que la contrasenia sea la misma.
+            if (contrasenia.equals(verificarContrasenia)){
+
+                //Objeto: Guardar las opciones ingresadas por el usuario.
+                ContentValues registro = new ContentValues();
+
+                //Guardar los datos en el objeto "registro".
+                registro.put("idUsuario", numUsuarios);
+                registro.put("nombreUsuario", nombreUsuario);
+                registro.put("correo", correo);
+                registro.put("contrasenia", contrasenia);
+                registro.put("tipo", tipoUsuario);
+
+                //Insertar los valores dentro de la tabla "usuarios".
+                BaseDatos.insert("usuarios", null, registro);
+
+                //Cerrar la Base de datos.
+                BaseDatos.close();
+
+                /*
+                String numU_S = Integer.toString(numUsuarios);
+
+                edt_nombreUsuario.setText(numU_S);
+                edt_correo.setText("");
+                edt_contrasenia.setText("");
+                edt_verificarContrasenia.setText("");
+                */
+
+                Toast.makeText(this, "Registro exitoso.", Toast.LENGTH_LONG).show();
+
+                //Permitir pasar a la siguiente actividad.
+                cumpleRequisitos = true;
+
+            }
+            //Solicitar que el usuario revise las contraseñas.
+            else{
+                Toast.makeText(this, "Las contraseñas tiene que ser iguales.", Toast.LENGTH_LONG).show();
+            }
+        }
+        //Solicitar al usuario que llene todos los campos.
+        else{
+            Toast.makeText(this, "Debes de llenar todos los campos", Toast.LENGTH_LONG).show();
+        }
+
     }
 
 }
--- app/src/main/java/com/example/oxytank_10/registrarCliente_comprobarDatos.java
+++ app/src/main/java/com/example/oxytank_10/registrarCliente_comprobarDatos.java
@@ -3,24 +3,85 @@ package com.example.oxytank_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class registrarCliente_comprobarDatos extends AppCompatActivity {
+
+    //Relación con el entorno grafico.
+    TextView nombreUsuario, correo, contrasenia, tipo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_cliente_comprobar_datos);
+
+        // Relación de las variables con la parte gráfica.
+        nombreUsuario = findViewById(R.id.tv_registrarCliente_comprobarDatos_usuario);
+        correo = findViewById(R.id.tv_registrarCliente_comprobarDatos_correo);
+        contrasenia = findViewById(R.id.tv_registrarCliente_comprobarDatos_contrasenia);
+        tipo = findViewById(R.id.tv_registrarCliente_comprobarDatos_tipo);
+
+        // Mostrar los datos.
+        mostrarDatos();
+
     }
 
     //Métodos.
 
     //Ir a la pantalla principal de la cuenta de tipo cliente.
     public void cuentaTipoCliente_PantallaPrincipal(View view){
+
+        //Recibir el ID ingresado por el usuario de "registrarCliente"
+        String usuario_id = getIntent().getStringExtra("usuario_id");
+        int id_usuario = Integer.parseInt(usuario_id);
+
         //Pasar a la actividad "cuentaTipoCliente_PantallaPrincipal".
         Intent Act_cuentaTipoCliente_PantallaPrincipal = new Intent(this, cuentaTipoCliente_PantallaPrincipal.class);
+        String IdComercio_String = Integer.toString(id_usuario);
+        Act_cuentaTipoCliente_PantallaPrincipal.putExtra("usuario_id", IdComercio_String);
         startActivity(Act_cuentaTipoCliente_PantallaPrincipal);
+
+    }
+
+    //Comprobar si los datos ingresados por el usuario de tipo comercio son correctos.
+    public void mostrarDatos(){
+
+
+        //Objeto: Administrar la base de datos.
+        DBHelper administrador = new DBHelper(this, "Oxytank_db", null, 1);
+
+        //Abrir Base de Datos
+        SQLiteDatabase BaseDatos = administrador.getWritableDatabase();
+
+        //Recibir el ID ingresado por el usuario de "registrarCliente"
+        String usuario_id = getIntent().getStringExtra("usuario_id");
+        int id_usuario = Integer.parseInt(usuario_id);
+
+        //Caso: Validar que los campos se encuentren llenos.
+        //Objeto: Seleccionar un el usuario correspondiente a los datos ingresados por el usuario en el registro del cliente.
+        Cursor fila_usuario = BaseDatos.rawQuery
+                ("select nombreUsuario, correo, contrasenia, tipo from usuarios " +
+                        "where idUsuario =" + id_usuario, null);
+
+
+        //Caso: Se encontro el usuario.
+        if(fila_usuario.moveToFirst()){
+
+            //Mostrar datos del usuario en la pantalla.
+            nombreUsuario.setText(fila_usuario.getString(0));
+            correo.setText(fila_usuario.getString(1));
+            contrasenia.setText(fila_usuario.getString(2));
+            tipo.setText(fila_usuario.getString(3));
+
+        }
+
+        //Cerrar la Base de datos.
+        BaseDatos.close();
+
     }
 
 }
