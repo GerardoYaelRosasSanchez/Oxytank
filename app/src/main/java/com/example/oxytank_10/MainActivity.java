c2397f7c98a6bb7e74fca5072e6f3c042b371eca
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-25 23:23:29 -0500

    Se hicieron avances en la pantalla de inicio de sesión.

--- app/src/main/java/com/example/oxytank_10/MainActivity.java
+++ app/src/main/java/com/example/oxytank_10/MainActivity.java
@@ -10,10 +10,25 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     //Variables para la relación con el entorno gráfico.
     EditText edt_nombreUsuario, edt_contrasenia;
+
+    // Guardar el ID correspondiente al usuario.
+    int numUsuarios;
+
+    //Arreglo "Usuarios_id"
+    int usuarios_id[] = new int[100];
+    String usuarios_nombre[] = new String[100];
+
+    //Verificar si existe el usuario.
+    Boolean existe = false;
+
+    //Guardar la posicion del id correspondiente al usuario.
+    int posicionUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +43,78 @@ public class MainActivity extends AppCompatActivity {
 
     //Métodos.
 
+    //Consultar todos los usuarios de la base de datos y guardarlos en el ArrayAdapter.
+    public void consultarUsuarios(View view){
+
+        int cont = 0;
+
+        String nombreUsuario_ingresado = edt_nombreUsuario.getText().toString();
+        String contrasenia_ingresado = edt_nombreUsuario.getText().toString();
+        
+        String contrasenia; 
+
+        //Objeto: Administrar la base de datos.
+        DBHelper admin = new DBHelper(this, "Oxytank_db", null, 1);
+
+        /// Abrir la base de datos en modo lectura y escritura.
+        SQLiteDatabase BaseDatos = admin.getWritableDatabase();
+
+        //Conseguir una lista de los datos de la tabla usuarios.
+        String datosUsuarios_Consulta = "SELECT idUsuario, nombreUsuario FROM usuarios"; // Guardar el texto que corresponde a la consulta.
+        Cursor cursor = BaseDatos.rawQuery(datosUsuarios_Consulta, null); //Realizar la consulta en la base de datos.
+        numUsuarios = cursor.getCount();
+
+        while (cursor.moveToNext()){
+            usuarios_id[cont] = cursor.getInt(0);
+            usuarios_nombre[cont] = cursor.getString(1);
+            cont += 1;
+        }
+
+        cont = 0;
+
+        cursor.close();
+
+
+
+        while (cont < numUsuarios){
+            String usuario = usuarios_nombre[cont];
+            if (usuario.equals(nombreUsuario_ingresado)){
+                existe = true;
+                posicionUsuario = cont;
+                cont = numUsuarios;
+            }
+            cont += 1;
+        }
+
+        if (existe){
+            Cursor fila_usuario = BaseDatos.rawQuery
+                    ("select contrasenia, tipo from usuarios " +
+                            "where idUsuario =" + usuarios_id[posicionUsuario], null);
+
+            //Caso: Se encontro el usuario.
+            if(fila_usuario.moveToFirst()){
+
+                //Mostrar datos del usuario en la pantalla.
+                String contrasenia = fila_usuario.getString(0);
+                String tipo = fila_usuario.getString(1);
+                
+                
+
+            }
+            
+        }
+        else{
+            Toast.makeText(this, "No existe el usuario.", Toast.LENGTH_LONG).show();
+        }
+
+        //Cerrar la Base de datos.
+        BaseDatos.close();
+        
+        existe = false;
+
+
+    }
+
     //Ir a la pantalla Seleccionar Tipo de Registro.
     public void seleccionarRegistro(View view){
         //Pasar a la actividad "seleccionarRegistro".
@@ -38,13 +125,15 @@ public class MainActivity extends AppCompatActivity {
     //Ir a la pantalla principal correspondiente al tipo de cliente que inicio sesión.
     public void pantallaPrincipal(View view){
 
-        iniciarSesion();
+        //iniciarSesion();
 
         //Pasar a la actividad "PantallaPrincipal".
         //Intent Act_PantallaPrincipal_cliente = new Intent(this, seleccionarRegistro.class);
         //startActivity(Act_PantallaPrincipal_cliente);
     }
 
+
+    /*
     //Realizar la consulta en la base de datos.
     public void iniciarSesion(){
 
@@ -74,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
                 String contrasenia_db = fila_cliente.getString(1);
                 String tipo_db = fila_cliente.getString(2);
 
-                /*
+
                 //Verificar que la contraseña ingresada en la base de datos se la misma
                 //que la ingresada por el usuario.
                 if (contrasenia_db.equals(contrasenia)){
@@ -82,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "Contraseña incorrecta.", Toast.LENGTH_LONG).show();
 
                 }
-                 */
+
 
             }
 
@@ -104,5 +193,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    */
 
 }
