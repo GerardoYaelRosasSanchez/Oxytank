d1129b2e7bd275223b44b3af51301a268969042d
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-30 16:00:01 -0500

    La pantalla principal de la cuenta de tipo comercio muestra correctamente el nombre de los comercios en el listView. Tambien se acomodaron loc comentarios y el nombre de las variables en el codigo.

--- app/src/main/java/com/example/oxytank_10/cuentaTipoCliente_PantallaPrincipal.java
+++ app/src/main/java/com/example/oxytank_10/cuentaTipoCliente_PantallaPrincipal.java
@@ -36,11 +36,11 @@ public class cuentaTipoCliente_PantallaPrincipal extends AppCompatActivity {
             "Sin servicio", "Sin servicio", "Sin servicio", "Sin servicio", "Sin servicio",
             "Sin servicio", "Sin servicio", "Sin servicio", "Sin servicio", "Sin servicio"};
 
-    // Guardar el numero de usuarios que contiene la tabla usuarios.
+    // Guardar el numero de comercios que contiene la tabla comercios.
     int numComercios = 0;
 
-    //Guardar el id del comercio y del usuario de la tabla comercios en una lista.
-    String comercio_nombre[] = new String[100];
+    //Guardar dar el nombre del comercio en una lista.
+    String lista_comercios_nombre[] = new String[100];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,24 +50,27 @@ public class cuentaTipoCliente_PantallaPrincipal extends AppCompatActivity {
         //Relación de las varaibles con el entorno gráfico.
         lv_listaComercios = findViewById(R.id.lv_cuentaTipoCliente_PantallaPrincipal_mostrarComercios);
 
+        //Llenar el array "lista_comercios_nombre" con la información de comercio.
         consultarComercio();
 
-        String prueba[] = new String[numComercios];
+        //Crear el arreglo con el que se mostraran los comercios del tamaño de comercios que existen.
+        String lv_listaComercios_nombre[] = new String[numComercios];
 
+        //Guardar los comercios en el array lv para mostrarlos en el listView.
         for (int i = 0; i < numComercios; i++){
-            prueba[i] = comercio_nombre[i];
+            lv_listaComercios_nombre[i] = lista_comercios_nombre[i];
         }
 
         //Caso: Si no existen comercios.
         if(numComercios == 0){
             //Se utiliza un adapter para mostrar la información por defaul en el listView.
             MyAdapter adapter = new MyAdapter(this, sinComercios);
-            lv_listaComercios.setAdapter(adapter);
+            lv_listaComercios.setAdapter(adapter); //Mostrar los datos en el listView.
         }
         //De lo contrario: Mostrar comercios en "ListView".
         else{
-            //Mostrar los negocios en la pantalla principal mediante ListView "lv_pantallaPrincipal_listaNegocios".
-            MyAdapter adapter = new MyAdapter(this, prueba);
+            //Mostrar los comercios en la pantalla principal mediante ListView "lv_cuentaTipoCliente_PantallaPrincipal_mostrarComercios".
+            MyAdapter adapter = new MyAdapter(this, lv_listaComercios_nombre);
             lv_listaComercios.setAdapter(adapter); //Mostrar los datos en listView.
 
             //Llevar al usuario a la pantalla correspondiente al comercio que selecciono.
@@ -83,7 +86,7 @@ public class cuentaTipoCliente_PantallaPrincipal extends AppCompatActivity {
 
     }
 
-    //Mostar los comerxios ingresados por el usuario de tipo comercio en "ListView".
+    //Guardar en una lista los datos del id, nombre y servicios de los comercios.
     public void consultarComercio(){
 
         int cont = 0; //Se utiliza para recorrer los arreglos y para realizar los ciclos en las funciones.
@@ -97,20 +100,18 @@ public class cuentaTipoCliente_PantallaPrincipal extends AppCompatActivity {
         //Buscar todos los nombres de la base de datos.
         Cursor cursor = BaseDatos.rawQuery("select * from comercios", null);
 
-        numComercios = cursor.getCount();
+        numComercios = cursor.getCount(); //Guardar cuantos comercios estan registrados.
 
         //Caso: Se encontraron los nombres.
         if(cursor.moveToFirst()){
             //Llenar el arreglo comercios.
             do{
-                comercio_nombre[cont] = cursor.getString(1);
+                lista_comercios_nombre[cont] = cursor.getString(1); //Guardar el nombre del comercio.
 
-                cont += 1;
+                cont += 1; //Pasar a la siguiente posición del arreglo.
 
-            }while (cursor.moveToNext());
+            }while (cursor.moveToNext()); //Pasar al siguiente elemento del cursos.
         }
-
-        cont = 0;
 
         //Cerrar el cursor.
         cursor.close();
@@ -136,7 +137,7 @@ public class cuentaTipoCliente_PantallaPrincipal extends AppCompatActivity {
 
         }
 
-        //Seleccionar los datos que se mostraran en el listView.
+        //Seleccionar los datos que se mostraran en el listView y mostrarlos.
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -146,9 +147,10 @@ public class cuentaTipoCliente_PantallaPrincipal extends AppCompatActivity {
             View fila = layoutInflater.inflate(R.layout.list_item_listacomercios,parent, false);
 
             //Relación de las variables con el entorno gráfico.
-            TextView comercios = fila.findViewById(R.id.tv_listItems_listaComercios_nombre);
+            TextView comercios_tv = fila.findViewById(R.id.tv_listItems_listaComercios_nombre);
 
-            comercios.setText(l_Comercios[position]);
+            //Mostrar la información.
+            comercios_tv.setText(l_Comercios[position]);
 
             return fila;
 
