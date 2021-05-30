32789761d39ff39c7484e122b8392b988fa68a06
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-30 01:14:43 -0500

    Se agrego el diseño y el funcionamiento de la pantalla principal de la cuenta cliente si aun no se ha registrado ningun comercio.

--- app/src/main/java/com/example/oxytank_10/cuentaTipoCliente_PantallaPrincipal.java
+++ app/src/main/java/com/example/oxytank_10/cuentaTipoCliente_PantallaPrincipal.java
@@ -1,27 +1,33 @@
 package com.example.oxytank_10;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class cuentaTipoCliente_PantallaPrincipal extends AppCompatActivity {
 
     //Relación de las variables con el entorno gráfico.
     ListView lv_listaComercios;
 
-    //Arreglo "Vacio"
+    //Arreglos que se utilizaran en el caso de que aún no exista ningun comercio registrado.
+        //Comercios.
     private String sinComercios[] = {"Sin comercios", "Sin comercios", "Sin comercios",
             "Sin comercios", "Sin comercios", "Sin comercios", "Sin comercios", "Sin comercios",
             "Sin comercios", "Sin comercios", "Sin comercios", "Sin comercios", "Sin comercios"};
-    private final String [][] datosComercio = {
-            {"Sin comercios", "Sin servicio"}, {"Sin comercios", "Sin servicio"}, {"Sin comercios", "Sin servicio"},
-            {"Sin comercios", "Sin servicio"}, {"Sin comercios", "Sin servicio"}, {"Sin comercios", "Sin servicio"},
-            {"Sin comercios", "Sin servicio"}, {"Sin comercios", "Sin servicio"}, {"Sin comercios", "Sin servicio"}
-    };
-
-    int [] lista_calificacion = {5, 2, 3, 2, 4, 5, 5, 5, 4};
+        //Servicios.
+    private String sinServicio[] = {"Sin servicio", "Sin servicio", "Sin servicio",
+            "Sin servicio", "Sin servicio", "Sin servicio", "Sin servicio", "Sin servicio",
+            "Sin servicio", "Sin servicio", "Sin servicio", "Sin servicio", "Sin servicio"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,29 +37,48 @@ public class cuentaTipoCliente_PantallaPrincipal extends AppCompatActivity {
         //Relación de las varaibles con el entorno gráfico.
         lv_listaComercios = findViewById(R.id.lv_cuentaTipoCliente_PantallaPrincipal_mostrarComercios);
 
+        //Se utiliza un adapter para mostrar la información por defaul en el listView.
+        MyAdapter adapter = new MyAdapter(this, sinComercios, sinServicio);
+        lv_listaComercios.setAdapter(adapter);
 
-        //Mostrar al usuario que aún no se ha registrado ningun comercio.
-        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_listacomercios, sinComercios);
-        //lv_listaComercios.setAdapter(new Adaptador_listaComercios(this, datosComercio, lista_calificacion));
+    }
 
-        lv_listaComercios.setAdapter(new Adaptador_listaComercios());
+    //Clase utilizada para la personalización y funcionamiento del adapter.
+    class MyAdapter extends ArrayAdapter<String> {
 
-        /*
-        //Caso: Si aun no se ha registrado ningun comercio.
-        if(comercios.size() == 0){
-            //Mostrar al usuario que aún no se ha registrado ningun comercio.
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_listacomercios, sinComercios);
-            lv_listaComercios.setAdapter(adapter);
+        Context context;
+        //Variables para guardar los datos que se utilizaran.
+        String l_sinComercios[];
+        String l_sinServicios[];
+
+        //Constructor.
+        MyAdapter (Context context, String sinComercio[], String sinServicio[]){
+            super(context, R.layout.list_item_listacomercios, R.id.tv_listItems_listaComercios_nombre, sinComercio);
+            this.context = context;
+            this.l_sinComercios = sinComercios;
+            this.l_sinServicios = sinServicio;
+
         }
-        //De lo contrario: Mostrar negocios en "ListView".
-        else{
 
-            //Mostrar los negocios en la pantalla principal mediante ListView "lv_pantallaPrincipal_listaNegocios".
-            //ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_listanegocios, negocios);
-            //lv_listaNegocios.setAdapter(adapter);
+        //Seleccionar los datos que se mostraran en el listView.
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+            //Relación del documento "list_item_listacomercios".
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View fila = layoutInflater.inflate(R.layout.list_item_listacomercios,parent, false);
+
+            //Relación de las variables con el entorno gráfico.
+            TextView comercios = fila.findViewById(R.id.tv_listItems_listaComercios_nombre);
+            TextView servicio = fila.findViewById(R.id.tv_listItems_listaComercios_servicioVenta);
+
+            //Mostrar los datos utilizando el formato personalizado.
+            comercios.setText(l_sinComercios[position]);
+            servicio.setText(l_sinServicios[position]);
+
+            return fila;
+
         }
-
-         */
-
     }
 }
