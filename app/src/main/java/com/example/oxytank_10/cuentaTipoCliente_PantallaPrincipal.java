e7d09db1b76257888b95bfb9c2188ba1336180ab
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-29 19:33:42 -0500

    Se agrego el diseño de la pantalla principal de tipo cliente.

--- app/src/main/java/com/example/oxytank_10/cuentaTipoCliente_PantallaPrincipal.java
+++ app/src/main/java/com/example/oxytank_10/cuentaTipoCliente_PantallaPrincipal.java
@@ -3,12 +3,20 @@ package com.example.oxytank_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class cuentaTipoCliente_PantallaPrincipal extends AppCompatActivity {
+
+    //Relación de las variables con el entorno gráfico.
+    ListView lv_listaComercios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuenta_tipo_cliente_pantalla_principal);
+
+        //Relación de las varaibles con el entorno gráfico.
+        lv_listaComercios = findViewById(R.id.lv_cuentaTipoCliente_PantallaPrincipal_mostrarComercios); 
+
     }
 }
