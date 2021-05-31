ab547755d5236dd63ece3ab6f8b573d24152e4ac
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-30 23:49:46 -0500

    Se agrego le diseño de la pamtalla modificar información de la cuenta de tipo comercio.

---
+++ app/src/main/java/com/example/oxytank_10/cuentaTipoComercio_modificarInformacion.java
@@ -0,0 +1,14 @@
+package com.example.oxytank_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cuentaTipoComercio_modificarInformacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuenta_tipo_comercio_modificar_informacion);
+    }
+}
