c2ab6ab13dcfc1089a244bca8c0e1fedc964ebf9
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-30 23:37:02 -0500

    Se agrego el diseño de la pantalla modificar informacion de la cuenta de tipo cliente.

---
+++ app/src/main/java/com/example/oxytank_10/cuentaTipoCliente_modificarInformacion.java
@@ -0,0 +1,14 @@
+package com.example.oxytank_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cuentaTipoCliente_modificarInformacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuenta_tipo_cliente_modificar_informacion);
+    }
+}
