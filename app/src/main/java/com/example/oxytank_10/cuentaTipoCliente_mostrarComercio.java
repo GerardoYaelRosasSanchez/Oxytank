8d60412220ccb847564748d1671922c269b16164
Gerardo Yael Rosas Sanchez <yael_rs2012@hotmail.com>
2021-05-30 19:20:42 -0500

    Se agrego la parte grafica de la pantalla ver comercio de la cuenta de tipo cliente.

---
+++ app/src/main/java/com/example/oxytank_10/cuentaTipoCliente_mostrarComercio.java
@@ -0,0 +1,17 @@
+package com.example.oxytank_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cuentaTipoCliente_mostrarComercio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuenta_tipo_cliente_mostrar_comercio);
+
+
+
+    }
+}
